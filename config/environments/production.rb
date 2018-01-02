902fea5968e125ea66898054268384f2b276512a
Dimitri Bosch <dimitribosch@hotmail.com>
2018-01-02 22:54:34 +0100

    config cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,6 +47,8 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.url = "wss://enigmatic-shelf-76947.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = [ "http://enigmatic-shelf-76947.herokuapp.com", "https://enigmatic-shelf-76947.herokuapp.com" ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
